8a78b5b62f9035aca0ce96d5da54151b33f6db05
JuntaoX <18jx10@queensu.ca>
2021-01-04 15:42:17 +0800

    rupdate

--- rename_file/main.py
+++ rename_file/main.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 
 def rename_file(dir, dstdir):
-    i = 3397     # new file will be named as number.jpg starting from 0
+    i = 0    # new file will be named as number.jpg starting from 0
     for file in tqdm(os.listdir(dir)):
         if file.endswith(".jpg"):
             shutil.copy(dir + "/" + file, dstdir + "/" + str(i) + ".jpg")
