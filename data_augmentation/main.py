9c94e54fd5dfc8ab58374c557d7ff488bee3ad8b
Your Name <xujuntao1616@outlook.com>
2021-01-09 23:18:05 +0800

    update

--- data_augmentation/main.py
+++ data_augmentation/main.py
@@ -4,48 +4,7 @@ import os
 from tqdm import tqdm
 
 
-def read(dir):
-    for name in tqdm(os.listdir(dir)):
-        if name.endswith(".jpg"):
-            print("name for img is " + name)
-            img = cv2.imread(dir + "/" + name)
-            # cv2.imshow("jpg", img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-            try:
-                txt_content = []
-                print("name for txt is " + name.split(".", -1)[0] + ".txt")
-                f = open(dir + "/" + name.split(".", -1)[0] + ".txt")
-                for line in f:
-                    txt_content.append(line.strip("\n"))
-                return img, txt_content
-            except:
-                print("cannot find correspondent txt for image")
-
-
-def main():
-    origin_dir = "E:/apple_quality_screening_release/v2.2/test_origin"
-    saving_dir = "E:/apple_quality_screening_release/v2.2/test_destination"
-    try:
-        os.chdir(origin_dir)
-        os.chdir(saving_dir)
-    except:
-        print("input directory not found")
-        main()
-
-    img, txt_content = read(origin_dir)
-    print(txt_content)
-
-    print("pick the strategy of data augmentation")
-    choices = ["crop_img_bboxes", "shift_pic_bboxes", "alterLight", "addNoise", "rotate_img_bboxes", "flip_pic_bboxes"]
-    i = 0
-    for choice in choices:
-        print("enter " + str(i) + " for " + choice)
-        i += 1
-
-    while True:
-        user_choice = input("which to perform: ")
-
+def process_with_choice(user_choice, img, txt_content):
         # crop_img_bboxes
         if user_choice == 0:
             data_augmentation.crop_img_bboxes()
@@ -64,7 +23,46 @@ def main():
         # flip_pic_bboxes
         elif user_choice == 5:
             data_augmentation.flip_pic_bboxes()
-        else:
-            continue
+
+
+def read(dir, user_choice):
+    for name in tqdm(os.listdir(dir)):
+        if name.endswith(".jpg"):
+            img = cv2.imread(dir + "/" + name)
+            # cv2.imshow("jpg", img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            f = open(dir + "/" + name.split(".", -1)[0] + ".txt")
+            txt_content = []
+            for line in f:
+                bbox = []
+                for coordinate in line.split(" "):
+                    bbox.append(coordinate.strip("\n"))
+                txt_content.append(bbox)
+            return img, txt_content
+
+
+def main():
+    origin_dir = "E:/apple_quality_screening_release/v2.2/test_origin"
+    saving_dir = "E:/apple_quality_screening_release/v2.2/test_destination"
+    try:
+        os.chdir(origin_dir)
+        os.chdir(saving_dir)
+    except:
+        print("input directory not found")
+        exit()
+
+    choices = ["crop_img_bboxes", "shift_pic_bboxes", "alterLight", "addNoise", "rotate_img_bboxes", "flip_pic_bboxes"]
+    user_choice = 0
+
+    if user_choice in [0, 1, 2, 3, 4, 5]:
+        print("Both input valid")
+    else:
+        print("invalid input")
+        exit()
+
+    img, txt_content = read(origin_dir, user_choice)
+    print(txt_content)
+
 
 main()
