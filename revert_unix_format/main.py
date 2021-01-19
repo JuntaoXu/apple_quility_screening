92b9576e6f1ee54d92e3eb26cd4a5e1729eb0e43
Your Name <xujuntao1616@outlook.com>
2021-01-19 23:41:59 +0800

    update

--- revert_unix_format/main.py
+++ revert_unix_format/main.py
@@ -1,21 +1,17 @@
 import os
+import sys
 
-# dir
-route = "G:/apple_quality_screening/apple_quality_screening_release/v2.2-tiny-linux/data"
-for root, dirs, files in os.walk(route):
-    for name in files:
-        # file type
-        if name.endswith(".txt") == "txt":
-            # combine dir with filename
-            catalog = os.path.join(root, name)
-            # replace all change line with "\n"
-            fp = open(catalog, "rU+")
-            # read file and save
-            strings = fp.read()
-            fp.close()
-            # write file with binary
-            fp1 = open(catalog, "wb")
-            fp1.seek(0)
-            fp1.write(strings)
-            fp1.flush()
-            fp1.close()
+
+def main(file_dir, cmd):
+    for filename in os.listdir(file_dir):
+        print(filename)
+        if filename.endswith(".txt"):
+            f = open(file_dir + filename, "w")
+            for line in f:
+                sys.stdout.write, (line[:-2] + '/n')
+
+file_path = "D:/darknet-master/apple_quality_screening_release/data_augmentation_origin/"
+cmd = "python -c " + "import sys; map(sys.stdout.write, (l[:-2] + '/n' for l in sys.stdin.readlines())) "
+print(cmd)
+
+main(file_path, cmd)
